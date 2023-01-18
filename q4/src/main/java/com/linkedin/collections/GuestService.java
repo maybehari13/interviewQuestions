56f1e0ecf7588a08d8fdef06bb120aced041b6e4
maybehari13 <39975818+maybehari13@users.noreply.github.com>
2023-01-18 12:31:29 -0600

    Add files via upload

---
+++ q4/src/main/java/com/linkedin/collections/GuestService.java
@@ -0,0 +1,44 @@
+package com.linkedin.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuestService {
+
+	private List<Guest> checkinList = new ArrayList<>(100);
+
+	public static List<Guest> filterByFavoriteRoom(List<Guest> guests, Room room) {
+
+		/*
+		 *  1. Returns a new collection that contains guests from the provided collection
+		 *  who have indicated the provided room as the first preference in their preferred
+		 *  room list. 
+		 */
+		
+		return null; 
+
+	}
+
+	public void checkIn(Guest guest) {
+		
+		/*
+		 *  2. Adds a guest to the checkinList, placing members of the loyalty program
+		 *  ahead of those guests not in the program. Otherwise, guests are arranged in the
+		 *  order they were inserted.
+		 */
+
+	}
+	
+	public void swapPosition(Guest guest1, Guest guest2) {
+		
+		/*
+		 *  3.  Swaps the position of the two provided guests within the checkinList.
+		 *  If guests are not currently in the list no action is required.
+		 */ 
+
+	}
+
+	public List<Guest> getCheckInList() {
+		return List.copyOf(this.checkinList);
+	}
+}
---
+++ q5/src/main/java/com/linkedin/collections/BookingService.java
@@ -0,0 +1,36 @@
+package com.linkedin.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BookingService {
+
+	private Map<Room, Guest> bookings = new HashMap<>();
+
+	public boolean book(Room room, Guest guest) {
+
+		/*
+		 * 1. The provided Guest is placed in the bookings Map and
+		 * associated with the provided room, only if no other guest
+		 * is associated with the room.
+		 * 
+		 * Returns a boolean that indicates if the Guest was
+		 * successfully placed in the room.
+		 */
+		
+		return false;
+	}
+
+	public double totalRevenue() {
+		
+		/*
+		 * 2. Returns a double that totals the rate of each Room booked
+		 * in the bookings Map.
+		 */
+		return 0;
+	}
+	
+	public Map<Room, Guest> getBookings() {
+		return bookings;
+	}
+}
